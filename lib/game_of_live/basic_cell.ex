41549ad429d69a9a0594ceea41837efa407b7a7d
chouzar <wychouza@gmail.com>
2019-06-13 01:25:10 -0500

    Added neighbour check functionality

--- lib/game_of_live/basic_cell.ex
+++ lib/game_of_live/basic_cell.ex
@@ -6,26 +6,57 @@ defmodule GameOfLive.BasicCell do
     {:ok, state}
   end
 
-  def handle_call(:are_you, _from, state = {life, _}) do
-    {:reply, life, state} 
+  def handle_cast({:setup, neighbours}, {life, _}) do
+    {:noreply, {life, neighbours}}
   end
 
-  def handle_cast(:live, state) do
-    send self(), :look_around
+  def handle_cast(:run, state) do
+    send(self(), :look_around)
     {:noreply, state}
   end
 
-  def handle_info(:look_around, {_, adjacent_cells}) do
-    life = Enum.random [:alive, :dead] 
-    IO.puts "#{inspect self()}, #{life}"
-    :erlang.send_after(1500, self(), :look_around) 
-    {:noreply, {life, adjacent_cells}} 
+  def handle_call(:are_you, _from, state = {life, _}) do
+    {:reply, life, state}
+  end
+
+  def handle_info(:look_around, {life, neighbours}) do
+    life = rules(life, count(neighbours))
+    IO.puts("#{inspect(self())}, #{life}")
+    :erlang.send_after(1500, self(), :look_around)
+    {:noreply, {life, neighbours}}
   end
 
   # Cell API
-  def start_link(), do: start_link({:dead, []})
-  def start_link(state), do: GenServer.start(__MODULE__, state)
+  def start_link(life), do: GenServer.start(__MODULE__, {life, []})
+  def setup(pid, neighbours), do: GenServer.cast(pid, {:setup, [neighbours]})
+  def run(pid), do: GenServer.cast(pid, :run)
   def are_you(pid), do: GenServer.call(pid, :are_you)
-  def live(pid), do: GenServer.cast(pid, :live)
+
+
+  defp count(neighbours) do
+      neighbours
+      |> Enum.map(fn pid -> are_you(pid) end)
+      |> Enum.filter(fn life -> life == :alive end)
+      |> Enum.count()
+  end
+
+  # Cell rules; may be implemented as a protocol 
+  # or anonymous function in the future, imagine
+  # passing the function as part of is behaviour :)
+  defp rules(:live, count)
+       when count < 2,
+       do: :dead
+  defp rules(:live, count)
+       when count == 2
+       when count == 3,
+       do: :alive
+  defp rules(:live, count)
+       when count > 3,
+       do: :dead
+  defp rules(:dead, count)
+       when count == 3,
+       do: :alive
+  defp rules(life, _count), do: life
+       do: life
 end
 
