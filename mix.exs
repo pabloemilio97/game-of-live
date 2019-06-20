55b30dcfae13b10907d7655bba1d7b4b492420e8
chouzar <wychouza@gmail.com>
2019-06-19 21:41:40 -0500

    Enforce quality checks

--- mix.exs
+++ mix.exs
@@ -7,7 +7,24 @@ defmodule GameOfLive.MixProject do
       version: "0.1.0",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      aliases: aliases(),
+      elixirc_options: [warnings_as_errors: true],
+      dialyzer: [
+        plt_file: {:no_warn, "priv/plts/dialyzer.plt"},
+        ignore_warnings: ".dialyzer_ignore.exs"
+      ]
+    ]
+  end
+
+  defp aliases do
+    [
+      quality: ["format", "dialyzer", "test"],
+      "quality.ci": [
+        "format --check-formatted",
+        "dialyzer --halt-exit-status",
+        "test"
+      ]
     ]
   end
 
@@ -21,8 +38,7 @@ defmodule GameOfLive.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:dialyxir, "~> 1.0.0-rc.6", only: [:dev], runtime: false}
     ]
   end
 end
