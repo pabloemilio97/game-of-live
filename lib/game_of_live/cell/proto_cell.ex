c41f305f005e61b81afca013b6a6499e2c0239ef
chouzar <wychouza@gmail.com>
2019-06-19 22:37:28 -0500

    Fixed some module name references

--- lib/game_of_live/cell/proto_cell.ex
+++ lib/game_of_live/cell/proto_cell.ex
@@ -1,29 +1,29 @@
 defmodule GameOfLive.Cell.ProtoCell do
-  import Rules.Conways
+  # import GameOfLive.Rules.Conways
 
-  def init(state = {life, neighbours, intelligence}) do
-    receive do
-      {:setup, neighbours} ->
-        init({life, neighbours, intelligence})
+  # def init(state = {life, neighbours, intelligence}) do
+  #   receive do
+  #     {:setup, neighbours} ->
+  #       init({life, neighbours, intelligence})
 
-      :run ->
-        send(self(), :look_around)
+  #     :run ->
+  #       send(self(), :look_around)
 
-      {:are_you, from} ->
-        send(from, life) 
+  #     {:are_you, from} ->
+  #       send(from, life)
 
-      :look_around ->
-        life = rules(life, count(neighbours))
-        :erlang.send_after(intelligence, self(), :look_around)
+  #     :look_around ->
+  #       life = rules(life, count(neighbours))
+  #       :erlang.send_after(intelligence, self(), :look_around)
+  #   end
 
-    end
-    init(state)
-  end
+  #   init(state)
+  # end
 
-  defp count(neighbours) do
-    neighbours
-    |> Enum.map(fn pid -> are_you(pid) end)
-    |> Enum.filter(fn life -> life == :alive end)
-    |> Enum.count()
-  end
+  # defp count(neighbours) do
+  #   neighbours
+  #   |> Enum.map(fn pid -> are_you(pid) end)
+  #   |> Enum.filter(fn life -> life == :alive end)
+  #   |> Enum.count()
+  # end
 end
